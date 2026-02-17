748b9f8b4bd06d52bc142bfcf805555d19e1120a
himasabry <36902869+himasabry@users.noreply.github.com>
2026-02-18 01:42:29 +0200

    Update channels.js

--- api/channels.js
+++ api/channels.js
@@ -1,7 +1,8 @@
-import fs from "fs";
-import path from "path";
-
 export default function handler(req, res) {
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+
   const filePath = path.join(process.cwd(), "data", "channels.json");
   const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
   res.status(200).json(data);
