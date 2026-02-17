cf8fd706848a380712062544389fdc75a912bac4
himasabry <36902869+himasabry@users.noreply.github.com>
2026-02-17 03:32:31 +0200

    Update proxy.m3u8.js

--- api/proxy.m3u8.js
+++ api/proxy.m3u8.js
@@ -4,29 +4,14 @@ export default async function handler(req, res) {
 
   const target = decodeURIComponent(url);
 
-  try {
-    const r = await fetch(target, {
-      headers: {
-        "User-Agent": "Mozilla/5.0",
-        "Referer": "",
-        "Origin": ""
-      }
-    });
+  const r = await fetch(target, {
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      "Referer": "",
+      "Origin": ""
+    }
+  });
 
-    let body = await r.text();
-
-    const base =
-      `${req.headers["x-forwarded-proto"] || "https"}://${req.headers.host}/api/proxy.m3u8?url=`;
-
-    // تحويل كل الروابط داخل m3u8 لتعدي من البروكسي
-    body = body.replace(/(https?:\/\/[^\s"'<>]+)/g, (match) => {
-      return base + encodeURIComponent(match);
-    });
-
-    res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
-    res.send(body);
-
-  } catch (e) {
-    res.status(500).send("Proxy Error");
-  }
+  res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
+  res.send(await r.text());
 }
