17705e6ba7f5ea9df42a595992cded9fe8eb8d2e
himasabry <36902869+himasabry@users.noreply.github.com>
2026-02-17 03:51:40 +0200

    Update play.m3u8.js

--- api/play.m3u8.js
+++ api/play.m3u8.js
@@ -1,12 +1,14 @@
 import fs from "fs";
 import path from "path";
+import fetch from "node-fetch";
 
 export default async function handler(req, res) {
   const { id } = req.query;
   if (!id) return res.status(400).send("Missing id");
 
-  const userAgent = req.headers['user-agent'] || '';
+  const userAgent = req.headers["user-agent"] || "";
   const REQUIRED_AGENT = "SUPER2026";
+
   if (!userAgent.includes(REQUIRED_AGENT)) {
     return res.status(403).send("Forbidden");
   }
@@ -22,5 +24,25 @@ export default async function handler(req, res) {
 
   if (!channel) return res.status(404).send("Channel not found");
 
-  res.redirect(channel.url);
+  // قناة ABR
+  if (channel.streams) {
+    res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
+
+    const host = `https://${req.headers.host}`;
+
+    return res.send(`#EXTM3U
+#EXT-X-VERSION:3
+
+#EXT-X-STREAM-INF:BANDWIDTH=6000000,RESOLUTION=3840x2160
+${host}/api/proxy.m3u8?url=${encodeURIComponent(channel.streams.high)}
+
+#EXT-X-STREAM-INF:BANDWIDTH=3000000,RESOLUTION=1920x1080
+${host}/api/proxy.m3u8?url=${encodeURIComponent(channel.streams.mid)}
+
+#EXT-X-STREAM-INF:BANDWIDTH=1200000,RESOLUTION=854x480
+${host}/api/proxy.m3u8?url=${encodeURIComponent(channel.streams.low)}
+`);
+  }
+
+  return res.redirect(channel.url);
 }
