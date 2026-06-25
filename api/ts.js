2176f580132bb9e2d39c2ae7f504989bf3819403
himasabry <36902869+himasabry@users.noreply.github.com>
2026-06-25 22:38:16 +0300

    Update ts.js

--- api/ts.js
+++ api/ts.js
@@ -2,17 +2,39 @@ import fetch from "node-fetch";
 
 export default async function handler(req, res) {
   try {
+
     const { url } = req.query;
-    if (!url) return res.status(400).send("Missing url");
+
+    if (!url) {
+      return res.status(400).send("Missing url");
+    }
 
     const upstream = await fetch(url, {
-      headers: { "User-Agent": "SUPER2026" }
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36"
+      }
     });
 
-    res.setHeader("Content-Type", "video/mp2t");
+    res.setHeader(
+      "Content-Type",
+      upstream.headers.get("content-type") ||
+      "video/mp2t"
+    );
+
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "*"
+    );
+
     upstream.body.pipe(res);
 
   } catch (e) {
-    res.status(500).send("TS Proxy Error");
+
+    console.error(e);
+
+    return res
+      .status(500)
+      .send("TS Proxy Error");
   }
 }
